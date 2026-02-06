ecbd8f62ad0a1dfa812dedb889396d8cb2dec114
pcdeb <gangulidebojit97@gmail.com>
2026-02-06 10:29:12 +0530

    Assignment-1_Task-2

---
+++ Assignment-1_Task-2.py
@@ -0,0 +1,9 @@
+'Create a personalised greeting'
+
+name = input("Enter your last name ")
+surname = input("Enter your last name: ")
+
+print("Hello deb"+name+" "+surname+"! Welcome to the Python program.")
+
+
+
